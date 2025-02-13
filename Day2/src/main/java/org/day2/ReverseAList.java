1e026d6620e23bed9e38f2a2cf04955311009f25
srijan0412 <srijan.soni01@gmail.com>
2025-02-13 10:19:11 +0530

    [Srijan] Add . Adding program files

---
+++ Day2/src/main/java/org/day2/ReverseAList.java
@@ -0,0 +1,47 @@
+package org.day2;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ReverseAList {
+    // Method to Reverse a LinkedList
+    public static void reverseALinkedList(LinkedList<Integer> array) {
+        int low = 0;
+        int high = array.size() - 1;
+        while (low < high) {
+            int temp = array.get(low);
+            array.set(low, array.get(high));
+            array.set(high, temp);
+            low ++;
+            high --;
+        }
+    }
+
+    // Method to Reverse a ArrayList
+    public static void reverseAArrayList(ArrayList<Integer> array) {
+        int low = 0;
+        int high = array.size() - 1;
+        while (low < high) {
+            int temp = array.get(low);
+            array.set(low, array.get(high));
+            array.set(high, temp);
+            low ++;
+            high --;
+        }
+    }
+    public static void main(String[] args) {
+        // Defining a sample Arraylist for testing
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        reverseAArrayList(arrayList);
+
+        // Ouptut for arrayList
+        System.out.println(arrayList);
+
+        // Defining a sample Linkedlist for testing
+        LinkedList<Integer> linkedList = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        reverseALinkedList(linkedList);
+        // Ouptut for arrayList
+        System.out.println(linkedList);
+    }
+}
---
+++ Day2/src/main/java/org/day2/RotatingAList.java
@@ -0,0 +1,37 @@
+package org.day2;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+public class RotatingAList {
+    // Method to rotate elements in the array k times
+    public static void rotateArray(ArrayList<Integer> array, int k) {
+        // Calculating the actual rotation count
+        k = k % array.size();
+
+        // Creating a sample list to store the elements that needs to be replaced
+        ArrayList<Integer> templist = new ArrayList<>();
+        for (int i = 0 ; i < k; i++) {
+            templist.add(array.get(i));
+        }
+
+        //Rotating the array
+        for (int i = k; i < array.size(); i++) {
+            array.set(i-k, array.get(i));
+        }
+        int idx = 0; // Iterator for iterating on the list
+        for (int i = array.size()-k; i < array.size(); i++) {
+            array.set(i, templist.get(idx));
+            idx++;
+        }
+    }
+    public static void main(String[] args) {
+        // Defining a test list to test the method
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        // Calling the rotateArray() method
+        rotateArray(arrayList, 2);  //Rotating by 2 places
+
+        // Displaying the output
+        System.out.println(arrayList);
+    }
+}

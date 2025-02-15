8ed0f1114e23c4ca16986783b76ef445420e495d
srijan0412 <srijan.soni01@gmail.com>
2025-02-15 12:07:52 +0530

    [Srijan] Add . Nested Try Catch Block

---
+++ Day4/src/main/java/org/day4/NestedTryCatchBlock.java
@@ -0,0 +1,31 @@
+package org.day4;
+
+import java.util.Scanner;
+
+public class NestedTryCatchBlock {
+    public static void main(String[] args) {
+        // Scanner class object for taking input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sample Array : {1, 2, 3, 4, 5}");
+
+        try {
+            // Sample array
+            int array[] = {1, 2, 3, 4, 5};
+
+            System.out.print("Enter Divisor: ");
+            int divisor = sc.nextInt();
+
+            try {
+                System.out.print("Enter index: ");
+                int idx = sc.nextInt();
+                System.out.println("Division : " + array[idx] / divisor); // Cause Arithmatic Exception & Index out of the bount exception
+            }
+            catch (ArithmeticException e) {
+                System.out.println("Divisor cant be zero!");
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Index");
+        }
+    }
+}

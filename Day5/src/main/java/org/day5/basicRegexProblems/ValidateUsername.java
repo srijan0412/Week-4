1e10fcd5d90aeafdfc74486bdd66b0d9030f3966
srijan0412 <srijan.soni01@gmail.com>
2025-02-15 16:04:21 +0530

    [Srijan] Add . Method to Validate Username

---
+++ Day5/src/main/java/org/day5/basicRegexProblems/ValidateUsername.java
@@ -0,0 +1,23 @@
+package org.day5.basicRegexProblems;
+
+import java.util.regex.*;
+
+public class ValidateUsername {
+    public static void main(String[] args) {
+        String inputs[] = {"user_123", "123user", "us"};
+        String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,14}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        for (String input : inputs) {
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches()) {
+                System.out.println(input + " - Valid");
+            }
+            else {
+                System.out.println(input + " - In-valid");
+            }
+        }
+
+
+    }
+}

f31d5ad69958f9e88c6e1d313565658d04a4498a
srijan0412 <srijan.soni01@gmail.com>
2025-02-17 10:15:48 +0530

    [Srijan] Add . Method to replace extra spaces with single space

---
+++ Day5/src/main/java/org/day5/basicRegexProblems/ReplaceSpaces.java
@@ -0,0 +1,21 @@
+package org.day5.basicRegexProblems;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class ReplaceSpaces {
+    public  static String replace(String regex, String input, String replacement) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.replaceAll(replacement);
+    }
+    public static void main(String[] args) {
+        // Regex pattern to match multiple spaces
+        String regex = "\\s+";
+        String testString = "This   is   a  test String.";
+
+        // Output
+        String result = replace(regex, testString, " ");
+        System.out.println(result);
+    }
+}

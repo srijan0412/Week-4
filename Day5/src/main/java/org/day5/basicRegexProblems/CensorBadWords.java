929fa6685b65d81a600ef957d24f27af6b00d022
srijan0412 <srijan.soni01@gmail.com>
2025-02-17 10:22:43 +0530

    [Srijan] Add . Censor bad words

---
+++ Day5/src/main/java/org/day5/basicRegexProblems/CensorBadWords.java
@@ -0,0 +1,27 @@
+package org.day5.basicRegexProblems;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CensorBadWords {
+    public  static String replace(String regex, String input, String replacement) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.replaceAll(replacement);
+    }
+    public static void main(String[] args) {
+        // Array of bad words
+        String badWords[] = {"damn", "stupid"};
+
+        // Regex pattern to match multiple spaces
+        String testString = "This is a damn bad example with some stupid words.";
+
+        // Censsor Bad Words
+        for (String badWord : badWords) {
+            testString = replace(badWord, testString, "****");
+        }
+
+        // Output
+        System.out.println(testString);
+    }
+}
